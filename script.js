113fabb68a10cb600379e7ad4c99751ea406f8a4
Aadish Shibu <144556449+Aadish680@users.noreply.github.com>
2025-02-10 11:09:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    const obj = [
+        ['M', 1000], ['CM', 900],
+        ['D', 500], ['CD', 400],
+        ['C', 100], ['XC', 90],
+        ['L', 50], ['XL', 40],
+        ['X', 10], ['IX', 9],
+        ['V', 5], ['IV', 4],
+        ['I', 1]
+    ];
 
-  //your code here
+    let result = "";
 
+    for (let i = 0; i < obj.length; i++) {
+        while (num >= obj[i][1]) {
+            result += obj[i][0]; // Append the Roman numeral
+            num -= obj[i][1]; // Subtract the corresponding value
+        }
+    }
+
+    return result;
 }
+
+// Example usage:
+console.log(convertToRoman(14));  // Output: "XIV"
+console.log(convertToRoman(798)); // Output: "DCCXCVIII"
+console.log(convertToRoman(3999)); // Output: "MMMCMXCIX"
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
